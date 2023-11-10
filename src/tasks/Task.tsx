2ae8205fb574de000f8c873d6ba9414b6de86d72
DinaLysenko <dinafate@rambler.ru>
2023-11-10 13:16:04 +0500

    first commit

---
+++ src/tasks/Task.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+type TaskType={
+    data: DataType
+}
+type DataType ={
+    title: string,
+    tasks: DataTaskType[],
+    students: string[]
+}
+type DataTaskType ={
+    taskId: number
+    title: string
+    isDone: boolean
+}
+export const Task:React.FC<TaskType> = ({data}) => {
+    return (
+        <div>
+            <h3>{data.title}</h3>
+            <ul>
+                {data.tasks.map(t=>{
+                    return (
+                        <li key={t.taskId}><input type={"checkbox"} checked={t.isDone}/> {t.title} </li>
+                    )
+                })}
+            </ul>
+            <ul>
+                {data.students.map((s, index)=>{
+                    return (
+                        <li key={index}>{s}</li>
+                    )
+                })}
+
+            </ul>
+        </div>
+        // <ul>
+        //     <li></li>
+        //     <li></li>
+        //     <li></li>
+        // </ul>
+    );
+};
+
